ab648fe2c15a42357719677a1518348acad24d66
dgw <dgw@technobabbl.es>
2019-08-25 17:58:18 -0500

    setup: configure entry-point in .cfg file

--- setup.py
+++ setup.py
@@ -16,9 +16,4 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     install_requires=requires,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
-    entry_points={
-        'sopel.plugins': [
-            'spellcheck = sopel_spellcheck',
-        ]
-    }
 )
