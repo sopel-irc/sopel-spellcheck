856d26dbbf862952ce67773913afe28df4e0fab7
dgw <dgw@technobabbl.es>
2019-12-14 20:07:05 -0600

    Version 0.0.2: Fix requirements
    We use setup.cfg now, instead of requirements.txt. Also, ignored the
    correct "egg-info" directory name in Git.

--- setup.py
+++ setup.py
@@ -6,14 +6,11 @@ def read_reqs(path):
         return list(fil.readlines())
 
 
-requires = read_reqs('requirements.txt')
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=requires,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
 )
